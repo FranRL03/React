04af665dc9556778f4fbaa4f97bc6c6a31410669
Fran Ruiz <fjavierrule@gmail.com>
2024-10-05 18:12:41 +0200

    prueba tecnica terminada

--- projects/react-prueba-tecnica/src/App.jsx
+++ projects/react-prueba-tecnica/src/App.jsx
@@ -1,43 +1,23 @@
-import { useState, useEffect } from "react"
+import './App.css'
+import { useCatImage } from './hooks/useCatImage.js'
+import { useCatFact } from './hooks/useCatFact.js'
 
-const CAT_PREFIX_IMAGE_URL = 'https://cataas.com/'
+export function App () {
+  const { fact, refreshFact } = useCatFact()
+  const { imageUrl } = useCatImage({ fact })
 
-export function App() {
+  const handleClick = async () => {
+    refreshFact()
+  }
 
-    const [fact, setFact] = useState()
-    const [imageUrl, setImageUrl] = useState()
+  return (
+    <main>
+      <h1>App de gatitos</h1>
 
-    // COMO QUEREMOS QUE ESTO SE RENDERIZE CUANDO SE INICIA LA PAGINA PUES PONEMOS EL ARRAY VACIO
-    useEffect(() => {
-        fetch('https://catfact.ninja/fact')
-            .then(res => res.json())
-            .then(data => {
-                const { fact } = data
-                setFact(fact)
+      <button onClick={handleClick}>Get new fact</button>
 
-                const threeFirstWord = fact.split(' ', 3).join(' ')
-                console.log(threeFirstWord)
-
-                fetch(`https://cataas.com/cat/says/${threeFirstWord}?fontSize=50&fontColor=red&json=true`)
-                    .then(res => res.json())
-                    .then(resposne => {
-                        console.log(resposne)
-                        const { url } = resposne
-                        console.log(`${CAT_PREFIX_IMAGE_URL}${imageUrl}`);
-                        setImageUrl(url)
-                    })
-            })
-    }, [])
-
-    return (
-
-        <main>
-            <h1>App de gatitos</h1>
-
-            {/* ESTO ES UN RENDERIZADO CONDICIONAL QUE HACE QUE SI "fact" TIENE UN VALOR
-            SE RENDERIZA ESA  LINEA SI NO TIENE VALOR PUES NO SE RENDERIZA */}
-            {fact && <p>{fact}</p>}
-            {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image exytracted using first three words for ${fact}`} />}
-        </main>
-    )
+      {fact && <p>{fact}</p>}
+      {imageUrl && <img src={imageUrl} alt={`Image extracted using the first three words for ${fact}`} />}
+    </main>
+  )
 }
